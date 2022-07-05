ad1782ababf2eba0dfe2fd6a1e72c39f695d9b49
Luis <luisgmsn2001@gmail.com>
2022-07-05 14:25:42 -0500

    Se agregó la barra de estatus con bootstrap e inicio de servidor de chat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { SocketIoConfig, SocketIoModule } from socke'ngx-t-io';
+const config: SocketIoConfig = 
+{
+  url:environment.wsUrl, options:{}
+};
+
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+import { FooterComponent } from './component/footer/footer.component';
+import { ChatComponent } from './component/chat/chat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    ChatComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SocketIoModule.forRoot(config)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/chat.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { WebsocketService } from './websocket.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ChatService {
+  constructor(public wsService: WebsocketService) {}
+
+  sendMessage(mensaje: String) {
+    const payload = {
+      de: 'Luis Alejandro',
+      cuerpo: mensaje,
+    };
+
+    this.wsService.emit('mensaje', payload);
+  }
+
+  getMessages() {
+   return this.wsService.listen('mensaje-nuevo');
+  }
+}
